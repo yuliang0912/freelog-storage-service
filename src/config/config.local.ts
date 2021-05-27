f76fe581c15fa43b24ff55838258235820635ba4
yuliang <yu.liang@freelog.com>
2021-05-27 09:51:41 +0800

    1.提交代码

--- src/config/config.local.ts
+++ src/config/config.local.ts
@@ -21,12 +21,16 @@ export default () => {
         url: 'mongodb://127.0.0.1:27017/storage'
     };
 
+    config.mongoose = {
+        url: `mongodb://storage_service:QzA4Qzg3QTA3NDRCQTA0NDU1RUQxMjI3MTA4ODQ1MTk=@dds-wz9ac40fee5c09441604-pub.mongodb.rds.aliyuncs.com:3717,dds-wz9ac40fee5c09442584-pub.mongodb.rds.aliyuncs.com:3717/test-storages?replicaSet=mgset-44484047`
+    };
+
     // config.mongoose = {
     //     url: 'mongodb://39.108.77.211:30772/storage'
     // };
 
     config.localIdentity = {
-        userId: 50028,
+        userId: 50017,
         username: 'yuliang'
     };
 
